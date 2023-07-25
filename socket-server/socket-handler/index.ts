65217d0efeb2564d71afd45503600dafd0f9787b
Badr <badrkmc@gmail.com>
2023-07-25 14:47:11 +0500

    add user join / create / leave socket events

--- socket-server/socket-handler/index.ts
+++ socket-server/socket-handler/index.ts
@@ -1,3 +1,4 @@
+import type { RoomInfo } from '$lib/types';
 import { Server } from 'socket.io';
 
 export default function socketHandler(server: any) {
@@ -8,15 +9,73 @@ export default function socketHandler(server: any) {
   });
   console.log("WebSocket server started");
 
+  const rooms: Map<string, string[]> = new Map();
+  const users: Map<string, string> = new Map();
+
   io.on('connection', (socket) => {
     console.log('User connected', socket.id);
+
+    // User disconnect
     socket.on('disconnect', () => {
       console.log('User disconnected', socket.id);
+      if (!users.has(socket.id))
+        return;
+      
+      // Broadcast to all users in room about user disconnection
+      const roomId = users.get(socket.id) as string;
+      io.to(roomId).emit('user-left', socket.id);
+
+      // Remove user from room
+      users.delete(socket.id);
+
+      // If only one user left in room, delete room
+      if (rooms.get(roomId)!.length === 1) {
+        console.log(`Room ${roomId} deleted`);
+        rooms.delete(roomId);
+        return;
+      }
+
+      rooms.set(roomId, rooms.get(roomId)!.filter((id) => id !== socket.id));
     });
     
+    // Ping from client
     socket.on('ping', (pingInformation) => {
       console.log(`Pings received from ${socket.id}`, pingInformation);
     });
     
+    // Create room
+    socket.on('create-room', () => {
+      console.log(`Room created by ${socket.id}`);
+      socket.join(socket.id);
+
+      rooms.set(socket.id, [socket.id]);
+      users.set(socket.id, socket.id);
+
+      socket.emit('room-created', socket.id);
+    });
+
+    // Join room
+    socket.on('join-room', (roomId) => {
+      if (!rooms.has(roomId)) {
+        console.log(`${socket.id} tried to join non-existent room ${roomId}`);
+        socket.emit('room-not-found', roomId);
+
+        return;
+      }
+
+      socket.join(roomId);
+      console.log(`${socket.id} joined room ${roomId}`);
+
+      rooms.get(roomId)!.push(socket.id);
+      users.set(socket.id, roomId);
+
+      console.log(rooms.get(roomId));
+      const roomInfo = {
+        roomId,
+        usersConnected: rooms.get(roomId)!.length
+      } as RoomInfo;
+      
+      io.to(roomId).emit('user-joined', roomInfo);
+    });
   });
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,16 +6,48 @@
   import serverLocations from '$lib/location-server';
   import { pingServer } from '$lib/ping-server';
 
+  import type { RoomInfo } from '$lib/types';
+
+  $: roomId = '';
+  $: roomJoined = false;
+  $: connectedUsers = 0;
   $: logs = [] as string[];
 
   onMount(() => {
+    // On connect
     socket.on('connect', () => {
       console.log('Connected from client');
+    });
+
+    // On room created
+    socket.on('room-created', (roomId: string) => {
+      console.log(`Room created: ${roomId}`);
+      console.log(`${socket.id} is now in room ${roomId}`)
+
+      roomJoined = true;
+      connectedUsers = 1;
+    });
+
+    // On user joined
+    socket.on('user-joined', (roomInfo: RoomInfo) => {
+      console.log(`User joined: ${roomInfo.roomId}`);
+      console.log(`${socket.id} is now in room ${roomInfo.roomId}`)
+
+      connectedUsers = roomInfo.usersConnected;
+      roomJoined = true;
+    });
+
+    // On user left
+    socket.on('user-left', (userId: string) => {
+      console.log(`User left: ${userId}`);
+
+      connectedUsers--;
     });
   });
 
   onDestroy(() => {
     socket.off('connect');
+    socket.off('room-created');
   });
 
   function sendPingInformation(logs: string[]) {
@@ -34,9 +66,37 @@
     logs = await Promise.all(pingPromises);
     sendPingInformation(logs);
   }
+
+  function joinRoom() {
+    if (roomId === '')
+      return;
+    
+    console.log("Joining room...");
+    socket.emit('join-room', roomId);
+  }
+
+  function createRoom() {
+    console.log("Creating room...")
+    socket.emit('create-room');
+  }
 </script>
 
-<button on:click={pingServers}>Ping all servers</button>
-{#each logs as log}
+{#if roomJoined}
+  <p>Connected users: {connectedUsers}</p>
+  <button on:click={pingServers}>Ping all servers</button>
+  {#each logs as log}
   <p>{log}</p>
-{/each}
+  {/each}
+{:else}
+  <form>
+    <label for="server-id">Room ID</label>
+    <input bind:value={roomId} type="text" id="server-id" name="server-id" />
+
+    <button on:click|preventDefault={joinRoom} type="submit">Join</button>
+  </form>
+
+  <form>
+    <label for="create-room">Create room</label>
+    <button name="create-room" on:click|preventDefault={createRoom} type="submit">Create</button>
+  </form>
+{/if}
