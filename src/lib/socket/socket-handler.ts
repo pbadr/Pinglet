d9f550ecd95d05f91ca96c37ea3e572bc17eb847
Badr <badrkmc@gmail.com>
2023-07-25 23:45:13 +0500

    change ip bind to all

--- src/lib/socket/socket-handler.ts
+++ src/lib/socket/socket-handler.ts
@@ -2,6 +2,6 @@ import ioClient from 'socket.io-client';
 
 const PORT = 5919;
 
-const socket = ioClient(`http://localhost:${PORT}`);
+const socket = ioClient(`http://0.0.0.0:${PORT}`);
 
 export default socket;
