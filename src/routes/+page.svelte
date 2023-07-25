9e161467f3fabd6172092ce21e5a42d1146b622a
Badr <badrkmc@gmail.com>
2023-07-25 13:25:34 +0500

    calculate ping

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,9 @@
 
   import { onMount } from 'svelte';
 
+  import serverLocations from '$lib/location-server';
+  import { pingServer } from '$lib/ping-server';
+
   onMount(() => {
     socket.on('connect', () => {
       console.log('Connected from client');
@@ -12,7 +15,15 @@
   function sendMessage() {
     socket.emit('message', 'Hello from client');
   }
+
+  function pingServers() {
+    Object.entries(serverLocations)
+    .forEach(([serverName, serverLocation]) => {
+      pingServer(serverName, serverLocation);
+    });
+  }
 </script>
 
 <h1>Send a message to client</h1>
 <button on:click={sendMessage}>Send</button>
+<button on:click={pingServers}>Ping all servers</button>
