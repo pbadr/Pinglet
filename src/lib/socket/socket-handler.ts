889b02c791600b24905f2f79296879dba26d5465
Badr <badrkmc@gmail.com>
2023-07-25 23:53:18 +0500

    use only URL for socket client

--- src/lib/socket/socket-handler.ts
+++ src/lib/socket/socket-handler.ts
@@ -2,8 +2,6 @@ import ioClient from 'socket.io-client';
 
 import { env } from '$env/dynamic/public'
 
-const PORT = 5919;
-
-const socket = ioClient(`${env.PUBLIC_BASE_URL}:${PORT}`);
+const socket = ioClient(`${env.PUBLIC_BASE_URL}`);
 
 export default socket;
