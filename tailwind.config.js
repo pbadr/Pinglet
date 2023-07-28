f2db8887f670991f6abf3bcd3b3398ef40c76777
Badr <badrkmc@gmail.com>
2023-07-28 14:19:57 +0500

    add custom bakbak one font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'bakbak-one': ['BakBak One', 'cursive'],
+      },
+    },
   },
   plugins: [],
 }
