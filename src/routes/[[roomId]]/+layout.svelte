37387daea404cee4c82532d36a8aecad58ea935a
Badr <badrkmc@gmail.com>
2023-07-29 20:34:10 +0500

    add github link for project

--- src/routes/[[roomId]]/+layout.svelte
+++ src/routes/[[roomId]]/+layout.svelte
@@ -1,5 +1,31 @@
 <script>
   import "../../app.css";
+
+  import github from "$lib/assets/github.svg";
 </script>
 
+
+<a href="https://github.com/pbadr/ping" target="_blank" ><img src={github} alt="GitHub logo" /></a>
 <slot />
+
+<style lang="postcss">
+  img {
+    @apply w-8 ml-4 mt-4;
+    position: absolute;
+    top: 0;
+    left: 0;
+    transition: opacity 200ms ease-in, transform 100ms ease-in;
+  }
+
+  img:hover {
+    @apply opacity-90 scale-110;
+  }
+
+  @media (max-width: 768px) {
+    img {
+      @apply ml-0 mr-4;
+      left: unset;
+      right: 0;
+    }
+  }
+</style>
